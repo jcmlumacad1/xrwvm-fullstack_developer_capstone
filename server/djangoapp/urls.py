2438ef08935e592f2b5d17fc9fecd26062489daa
jcmlumacad1 <44424424+jcmlumacad1@users.noreply.github.com>
2024-09-14 15:39:40 +0800

    integrate with rest api

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
 
     # path for add a review view
+    path(route='add_review', view=views.add_review, name='add_review'),
     
     path(route='get_cars', view=views.get_cars, name ='getcars'),
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
