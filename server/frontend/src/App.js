c13badcb8ba67c35fd52891e70a087bb965babf9
jcmlumacad1 <44424424+jcmlumacad1@users.noreply.github.com>
2024-09-14 16:02:50 +0800

    fix import of Dealers

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,14 +1,14 @@
 import LoginPanel from "./components/Login/Login";
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register/Register";
-import Dealer from "./components/Dealers/Dealer";
+import Dealer from "./components/Dealers/Dealers";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dealers" element={<Dealer />} />
+      <Route path="/dealers" element={<Dealers />} />
     </Routes>
   );
 }
