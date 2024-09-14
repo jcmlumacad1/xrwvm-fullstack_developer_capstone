4a46a7b5e981aa94cea790ba527c17d8636dc78a
jcmlumacad1 <44424424+jcmlumacad1@users.noreply.github.com>
2024-09-14 15:56:00 +0800

    display dealers

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 from . import views
 
 app_name = 'djangoapp'
@@ -23,5 +24,6 @@ urlpatterns = [
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
