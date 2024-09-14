0d15737f8dabd99eb5dfd5970beba6fd2c925e80
jcmlumacad1 <44424424+jcmlumacad1@users.noreply.github.com>
2024-09-14 16:03:02 +0800

    fix import of Dealers

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,7 +1,7 @@
 import LoginPanel from "./components/Login/Login";
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register/Register";
-import Dealer from "./components/Dealers/Dealers";
+import Dealers from "./components/Dealers/Dealers";
 
 function App() {
   return (
